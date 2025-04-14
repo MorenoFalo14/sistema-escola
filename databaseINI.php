aedccd6c9fb1eb16fe8e72ce43d277026be95d5e
MorenoFalo14 <morenofalo5@gmail.com>
2025-04-14 20:27:36 +0100

    Update databaseINI.php

--- databaseINI.php
+++ databaseINI.php
@@ -41,13 +41,13 @@ $execucao3 = mysqli_query($conexao, $query3);
 $query = "CREATE TABLE notas (id_nota int not null auto_increment primary key, id_aluno_curso int not null, descricao varchar(40), nota_valor decimal(10,2))";
 $execucao = mysqli_query($conexao, $query); 
 
-$query = "INSERT INTO alunos (nome, nascimento, bairro) values ('Carla Matos', '2000-03-15','Se'), ('Lucas Seixas', '1997-01-29','Liberdade'), ('Jessica Brito', '1998-02-02','Pinheiros'), ('Victor Silveira', '1996-05-07','Vila Mariana'), ('Luciano Rocha', '1999-07-23','Vila Prudente')";
+$query = "INSERT INTO alunos (nome_aluno, nascimento, bairro) values ('Carla Matos', '2000-03-15','Se'), ('Lucas Seixas', '1997-01-29','Liberdade'), ('Jessica Brito', '1998-02-02','Pinheiros'), ('Victor Silveira', '1996-05-07','Vila Mariana'), ('Luciano Rocha', '1999-07-23','Vila Prudente')";
 $execucao = mysqli_query($conexao, $query); 
 
 $query = "INSERT INTO aluno_curso (id_aluno, id_curso) values (3,3), (4,4), (5,1), (6,2), (7,3)";
 $execucao = mysqli_query($conexao, $query); 
 
-$query = "INSERT INTO notas (id_aluno_curso, descricao, nota_valor) values (1, 'P1', 8.7), (2, 'P1', 0.7)(3, 'P1', 7.2),(4, 'P1', 3.7),(5, 'P1', 5.0),(6, 'P1', 6.1),(7, 'P1', 9.8)";
+$query = "INSERT INTO notas (id_aluno_curso, descricao, nota_valor) values (1, 'P1', 8.7), (2, 'P1', 0.7),(3, 'P1', 7.2),(4, 'P1', 3.7),(5, 'P1', 5.0),(6, 'P1', 6.1),(7, 'P1', 9.8)";
 $execucao = mysqli_query($conexao, $query);
 
 $query = "CREATE TABLE usuarios (usuario varchar(40) not null primary key unique, senha varchar(40));"
