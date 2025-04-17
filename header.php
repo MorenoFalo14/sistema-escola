e86a24bf459ece28659c35cdced834b34473d609
MorenoFalo14 <morenofalo5@gmail.com>
2025-04-17 19:34:23 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="shortcut icon" href="imgs/dolphin1.ico" type="image/x-icon" />
-		<title> Sistema de Ensino Golfinho</title>
+		<link rel="shortcut icon" href="imgs/imtsb_logo.jpg" type="image/x-icon" />
+		<title> INSTITUTO MÉDIO TÉCNICO SÃO BENEDITO</title>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="css/bootstrap.min.css"> 
 		<link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-		<link rel="stylesheet" href="sistema-escola/css/style">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 
 
 	<body>
 		<header>
-			<a <?php if(isset($_SESSION['login'])){ ?> href="?pagina=seg" <?php } else { ?> href="?pagina=home" <?php } ?> > <img src="imgs/dolphin1.png"> </a>
+			<a <?php if(isset($_SESSION['login'])){ ?> href="?pagina=seg" <?php } else { ?> href="?pagina=home" <?php } ?> > <img src="imgs/IMTSB_Logo_m.png"> </a>
 			
 
 			<div id="menu" >
